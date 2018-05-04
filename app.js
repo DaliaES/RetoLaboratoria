702f2a5cd3a8414b32a4a14314c0d652631c06ff
dalia <dalia.escuderov@gmail.com>
2018-05-03 22:49:37 -0500

    primer script

--- app.js
+++ app.js
@@ -35,3 +35,54 @@ function categoria_cuatro(){
   var categoria = document.getElementById("peliculas");
   categoria.style.display = "block";
 }
+// validar
+function respuestadep(){
+  var valida="0";
+  var respuesta_uno=false;
+  var contador_respuesta = 0;
+  //preg 1
+    var porNombre=document.getElementsByName("dep");
+    for(var i=0;i<porNombre.length;i++)
+    {
+        if(porNombre[i].checked){
+          valida=porNombre[i].value;
+          if(valida=="1"){
+              respuesta_uno=true;
+              contador_respuesta++;
+          }
+        }
+    }
+    //pregunta2
+    valida="0";
+    var respuesta_dos=false;
+    var porNombre=document.getElementsByName("dep2");
+    for(var i=0;i<porNombre.length;i++)
+    {
+        if(porNombre[i].checked){
+          valida=porNombre[i].value;
+          if(valida=="1"){
+              respuesta_dos=true;
+              contador_respuesta++;
+          }
+        }
+    }
+// preg 3
+valida="0";
+var respuesta_tres=false;
+var porNombre=document.getElementsByName("dep3");
+for(var i=0;i<porNombre.length;i++)
+{
+    if(porNombre[i].checked){
+      valida=porNombre[i].value;
+      if(valida=="1"){
+          respuesta_tres=true;
+          contador_respuesta++;
+      }
+    }
+}
+    if (respuesta_uno & respuesta_dos & respuesta_tres) {
+      alert ("Felicitaciones")
+    } else {
+      alert (' Tiene: '+ contador_respuesta +' Suerte para la proxima ');
+    }
+}
